049f6651071377b15dbb156ee8e681ea4bf64797
serhiichaiuk <chayck7@gmail.com>
2022-07-30 11:10:09 +0300

    display songs

--- src/components/CurrentSong.vue
+++ src/components/CurrentSong.vue
@@ -2,11 +2,14 @@
   <div>
     <div class="currentsong sticky-top">
       <div class="currentsong-background position-relative">
-        <div class="currentsong-backgroundimage position-absolute"></div>
+        <div
+          class="currentsong-backgroundimage position-absolute"
+          :style="{ backgroundImage: 'url(' + song.thumb + ')' }"
+        ></div>
         <div
           class="song-info d-flex justify-content-center align-items-center py-5"
         >
-          <div class="item container">
+          <div class="item container position-relative">
             <div class="row justify-content-center">
               <div class="song col-md-8 text-center">
                 <h1 class="song-name">{{ song.name }}</h1>
